2aba7a882c190162b0ec85bff86e94f2b6c589ac
radek <kedar03@gmail.com>
2018-02-12 07:15:59 +0100

    Add report printing

--- page_report.py
+++ page_report.py
@@ -4,6 +4,8 @@ from http import HTTPStatus
 from enum import IntEnum
 from urllib.parse import urlparse
 import time
+import os
+import sys
 
 class LinePatternChecker:
 
@@ -48,7 +50,10 @@ class LinePatternChecker:
 
 def get_address(url):
     parsed_url = urlparse(url)
-    return parsed_url.netloc
+    path = parsed_url.path
+    if path and path[len(path) - 1] == '/':
+        path = path[:-1]
+    return parsed_url.netloc + path
 
 def is_valid_http_code(code):
     int_http_response_codes = list(map(int, HTTPStatus))
@@ -69,8 +74,50 @@ def is_valid_ipv4_address(ip):
         return False
     return True
 
+def line_valid(groups):
+    if not groups:
+        return False
+    address = get_address(groups[LinePatternChecker.address_name_group])
+    ip_valid =  is_valid_ipv4_address(groups[LinePatternChecker.ip_name_group])
+    datetime_valid = is_valid_datetime(groups[LinePatternChecker.datetime_name_group])
+    http_code_valid = is_valid_http_code(groups[LinePatternChecker.response_code_name_group])
+    if address and ip_valid and datetime_valid and http_code_valid:
+        return True
+    return False
+
+def get_groups(pattern_iter):
+    groups = {}
+    for item in pattern_iter:
+        groups.update(item.groupdict())
+    return groups
+
 def parse():
+    if len(sys.argv) != 2:
+        sys.stderr.write("Missing file name\n")
+        return
+    filename = sys.argv[1]
     pattern = LinePatternChecker.build_pattern()
-    requests = ["10.4.180.222 [28/Jan/2018:10:02:32 +0100] \"GET http://clearcode.cc/ HTTP/1.1\" 200 1080"]
-    groups = [x.groupdict() for x in pattern.finditer(requests[0])]
-    print(groups)
+    
+    invalid_lines = 0
+    report = {}
+    with open(filename) as log:
+        for line in log:
+            groups = get_groups(pattern.finditer(line))
+            if not line_valid(groups):
+                invalid_lines += 1
+                continue
+            address = get_address(groups[LinePatternChecker.address_name_group])
+            if address in report:
+                report[address] += 1
+            else:
+                report[address] = 1
+
+    sorted_report = sorted(report.items(), key=lambda x: (-x[1], x[0]))
+    
+    for key, value in sorted_report:
+        sys.stdout.write("\"%s\",%d\n"%(key,report[key]))
+
+    if invalid_lines != 0:
+        sys.stderr.write("Invalid log lines: %s\n"%str(invalid_lines))
+
+parse()
